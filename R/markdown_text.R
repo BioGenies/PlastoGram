28d30f0d7819ed584196c29168927dccc32db61d
michbur <michalburdukiewicz@gmail.com>
2022-08-10 17:52:32 +0200

    fixed funding info

--- R/markdown_text.R
+++ R/markdown_text.R
@@ -16,9 +16,7 @@ For general questions or problems, please email
 
 markdown_acknowledgements <- function() {
   "
-  This work
-has been supported by the Foundation of Polish Science grant TEAM TECH CORE FACILITY/2016-2/2 to M.B, 
-the National Science Centre grant [2018/31/N/NZ2/01338](https://projekty.ncn.gov.pl/index.php?projekt_id=429890) to K.S., 
+  This work has been supported by the National Science Centre grant [2018/31/N/NZ2/01338](https://projekty.ncn.gov.pl/index.php?projekt_id=429890) to K.S., 
 the National Science Centre grant [2017/26/D/NZ8/00444](https://projekty.ncn.gov.pl/index.php?projekt_id=384760) to P.G.,
 and the National Science Centre grant [2019/35/N/NZ8/03366](https://projekty.ncn.gov.pl/index.php?projekt_id=463135) to F.P.
   "
